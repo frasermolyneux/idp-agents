16c1d518a10237416a6ec9f642a3139dd6fde591
Fraser Molyneux <frasermolyneux@mx-mail.io>
2026-03-03 13:50:05 +0000

    feat: Add role assignments for storage table and queue contributors

--- terraform/role_assignments.tf
+++ terraform/role_assignments.tf
@@ -9,3 +9,15 @@ resource "azurerm_role_assignment" "function_app_storage_account_contributor" {
   role_definition_name = "Storage Account Contributor"
   principal_id         = local.agents_identity_principal_id
 }
+
+resource "azurerm_role_assignment" "function_app_storage_table_contributor" {
+  scope                = azurerm_storage_account.function_app_storage.id
+  role_definition_name = "Storage Table Data Contributor"
+  principal_id         = local.agents_identity_principal_id
+}
+
+resource "azurerm_role_assignment" "function_app_storage_queue_contributor" {
+  scope                = azurerm_storage_account.function_app_storage.id
+  role_definition_name = "Storage Queue Data Contributor"
+  principal_id         = local.agents_identity_principal_id
+}
